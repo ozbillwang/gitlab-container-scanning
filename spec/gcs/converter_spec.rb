b574907e8d0e7d41dbfeb48b7d854d2e47fae426
can eldem <eldemcan@gmail.com>
2020-12-05 17:13:17 +0000

    Add more fixtures for different image types

--- spec/gcs/converter_spec.rb
+++ spec/gcs/converter_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe Gcs::Converter do
-  let(:trivy_output) { fixture_file_content('trivy-alpine.json') }
+  let(:trivy_output_alpine) { fixture_file_content('trivy-alpine.json') }
+  let(:trivy_output_centos) { fixture_file_content('trivy-centos.json') }
+  let(:trivy_output_debian) { fixture_file_content('trivy-debian.json') }
 
   IdentifierSchema = Dry::Schema.JSON do
     required(:type).filled(:string)
@@ -44,8 +46,21 @@ RSpec.describe Gcs::Converter do
   end
 
   it 'converts into valid format' do
-    gitlab_format = described_class.new(trivy_output, nil, {}).convert
+    gitlab_format = described_class.new(trivy_output_alpine, nil, {}).convert
     result = schema.call(gitlab_format)
-    expect(result.errors.empty?).to be_truthy
+    expect(result.success?).to be_truthy
+  end
+
+  it 'converts into valid format for centos' do
+    gitlab_format = described_class.new(trivy_output_centos, nil, {}).convert
+    result = schema.call(gitlab_format)
+    expect(result.success?).to be_truthy
+  end
+
+  it 'converts into valid format for debian based images' do
+    gitlab_format = described_class.new(trivy_output_debian, nil, {}).convert
+    result = schema.call(gitlab_format)
+
+    expect(result.success?).to be_truthy
   end
 end
