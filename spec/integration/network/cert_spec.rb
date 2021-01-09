ff9c848eaaba013bd1b96a493655e306511e1f4a
can eldem <eldemcan@gmail.com>
2021-01-09 19:23:57 +0000

    add project file for offline env test v2

--- spec/integration/network/cert_spec.rb
+++ spec/integration/network/cert_spec.rb
@@ -2,14 +2,20 @@
 
 RSpec.describe 'alpine' do
   context 'when scanning an Alpine based image', integration: true do
-    include_examples 'as container scanner'
+    let(:pwd) { Pathname.new(File.dirname(__FILE__)).join('../../..') }
 
-    let(:project_fixture) { fixture_file('docker/alpine_project') }
-    let(:env) do
-      {
-        'ADDITIONAL_CA_CERT_BUNDLE' => x509_certificate.read,
-        'DOCKER_IMAGE' => 'docker.test/library/alpine:latest'
-      }
+    subject { runner.report_for(type: 'container-scanning') }
+
+    before(:all) do
+      runner.mount(dir: fixture_file('docker/alpine_project'))
+      runner.scan(
+        env: {
+          'ADDITIONAL_CA_CERT_BUNDLE' => x509_certificate.read,
+          'DOCKER_IMAGE' => 'docker.test/library/alpine:latest'
+        }
+      )
     end
+
+    specify { expect(subject).to match_schema(:container_scanning) }
   end
 end
