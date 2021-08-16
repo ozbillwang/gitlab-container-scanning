7f33d5b7e4b6db2be27aa0b909c10fafccdc5d2b
Thiago Figueiró <tfigueiro@gitlab.com>
2021-08-16 14:15:59 +1000

    Remediation spec coverage

--- spec/gcs/remediations/collection_spec.rb
+++ spec/gcs/remediations/collection_spec.rb
@@ -3,15 +3,58 @@
 RSpec.describe Gcs::Remediations::Collection do
   let(:remediation_collection) { described_class.new }
 
+  describe '#disabled?' do
+    context 'when docker_file exists' do
+      let(:docker_file) { fixture_file('docker/remediation-Dockerfile') }
+
+      it 'returns false' do
+        expect(described_class.new(docker_file).disabled?).to be false
+      end
+    end
+
+    context 'when docker_file is missing' do
+      let(:docker_file) { Pathname.new('/path/to/some/non-existing/file') }
+
+      it 'returns true' do
+        expect(described_class.new(docker_file).disabled?).to be true
+      end
+    end
+  end
+
   describe '#create_remediation' do
+    let(:vulnerability) { JSON.parse(fixture_file_content('report.json'))['vulnerabilities'][0] }
+    let(:converted_vuln) { Gcs::Vulnerability.new(vulnerability) }
+
     subject { remediation_collection.create_remediation(converted_vuln, vulnerability) }
 
+    it 'checks whether remediations are disabled' do
+      expect(remediation_collection).to receive(:disabled?).once
+      subject
+    end
+
     context 'when OS is unsupported' do
-      let(:converted_vuln) { nil }
-      let(:vulnerability) { nil }
+      let(:vulnerability) { JSON.parse(fixture_file_content('trivy-unsupported-os.json'))['vulnerabilities'][0] }
+
+      before do
+        subject
+      end
 
       it 'skips remediation' do
-        expect(remediation_collection.remediations).to be_empty
+        expect(remediation_collection.to_hash).to be_empty
+      end
+
+      it 'adds OS to unsupported list' do
+        expect(remediation_collection.unsupported_operating_systems).to include("unsupported-os 1.12")
+      end
+    end
+
+    context 'when OS is supported' do
+      before do
+        subject
+      end
+
+      it 'adds remediation' do
+        expect(remediation_collection.to_hash[0]).to include(summary: 'Upgrade apt to 1.4.9')
       end
     end
   end
