78b16c8a4e178eae25a305d7cdb0cba0edeaed32
Sashi <skumar@gitlab.com>
2021-10-28 13:11:36 +0530

    Checkout modified dockerfile

--- spec/gcs/remediations/collection_spec.rb
+++ spec/gcs/remediations/collection_spec.rb
@@ -52,6 +52,10 @@ RSpec.describe Gcs::Remediations::Collection do
       let(:docker_file) { fixture_file('docker/remediation-Dockerfile') }
       let(:remediation_collection) { described_class.new(docker_file) }
 
+      after do
+        `git checkout #{docker_file.to_path}`
+      end
+
       before do
         create_remediation
       end
