49fe725a63e0b3ee782e65a1c8d13ef322242503
Alan (Maciej) Paruszewski <mparuszewski@gitlab.com>
2022-02-03 20:57:43 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- lib/gcs/remediations/remediation.rb
+++ lib/gcs/remediations/remediation.rb
@@ -19,20 +19,25 @@ module Gcs
       }.freeze
 
       PACKAGE_MANAGER_MAPPINGS = {
+        # Both
         'debian' => 'apt',
         'ubuntu' => 'apt',
         'alpine' => 'apk',
         'photon' => 'tdnf',
-        'amzn' => 'yum',
+
+        # Trivy
         'amazon' => 'yum',
         'centos' => 'yum',
-        'ol' => 'yum',
-        'oracle' => 'yum',
-        'rhel' => 'yum',
-        'redhat' => 'yum',
         'opensuse' => 'zypper',
-        'opensuseleap' => 'zypper',
-        'opensuse.leap' => 'zypper'
+        'oracle' => 'yum',
+        'redhat' => 'yum',
+        'opensuse.leap' => 'zypper',
+
+        # Grype
+        'amzn' => 'yum',
+        'ol' => 'yum',
+        'rhel' => 'yum',
+        'opensuseleap' => 'zypper'
       }.freeze
 
       Fixes = Struct.new(:cve, :id) do
