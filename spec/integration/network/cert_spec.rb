23beb3ad22fe7143d325ad079fc69d1252ba0f74
Alan (Maciej) Paruszewski <mparuszewski@gitlab.com>
2022-02-11 20:26:39 +0100

    Fix ca certificate tests

--- spec/integration/network/cert_spec.rb
+++ spec/integration/network/cert_spec.rb
@@ -1,26 +1,24 @@
 # frozen_string_literal: true
 
-RSpec.describe 'alpine' do
+RSpec.describe 'ca cert' do
   before(:all) do
     setup_schemas!
   end
 
-  context 'when scanning an Alpine based image', integration: :ca_cert do
+  context 'when scanning an image with ca certificate bundle', integration: :ca_cert do
     subject(:report) { runner.report_for(type: 'container-scanning') }
 
-    let(:env) do
-      {
-        'ADDITIONAL_CA_CERT_BUNDLE' => x509_certificate.read,
-        'DOCKER_IMAGE' => 'docker.test/library/alpine:latest',
-        'DOCKER_USER' => '',
-        'DOCKER_PASSWORD' => '',
-        'CS_DISABLE_DEPENDENCY_LIST' => 'false'
-      }
-    end
-
     before(:all) do
-      runner.mount(env: env)
-      runner.scan(env: env)
+      runner.mount(env: { 'DOCKER_IMAGE' => 'alpine:3.12.0' })
+      runner.scan(
+        env: {
+          'ADDITIONAL_CA_CERT_BUNDLE' => x509_certificate.read,
+          'DOCKER_IMAGE' => 'docker.test/library/alpine:3.12.0',
+          'DOCKER_USER' => '',
+          'DOCKER_PASSWORD' => '',
+          'CS_DISABLE_DEPENDENCY_LIST' => 'false'
+        }
+      )
     end
 
     specify { expect(report).to match_schema(:container_scanning) }
