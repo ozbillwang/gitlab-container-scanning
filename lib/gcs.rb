47a3b6e373a20fc5355132a789e8ba027fd3016a
Dominic Bauer <dbauer@gitlab.com>
2022-04-06 15:23:37 +0200

    Enable OpenSSL FIPS mode conditionally

--- lib/gcs.rb
+++ lib/gcs.rb
@@ -18,7 +18,7 @@ require 'yaml'
 require 'term/ansicolor'
 require 'openssl'
 
-OpenSSL.fips_mode = true
+OpenSSL.fips_mode = true if Gcs::Environment.ubi?
 
 loader = Zeitwerk::Loader.for_gem
 loader.push_dir(File.join(__dir__, '../ee/lib'))
