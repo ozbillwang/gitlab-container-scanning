98c816876a77f70802df236bc044466f15680b65
Sashi <skumar@gitlab.com>
2022-05-09 20:31:39 +0530

    Fix style error

--- ee/spec/spec_helper.rb
+++ ee/spec/spec_helper.rb
@@ -15,7 +15,7 @@ RSpec.configure do |config|
   config.extend EnvironmentHelper::ClassMethods
   config.include SchemaHelper
 
-  config.around(:example) do |example|
+  config.around do |example|
     with_modified_environment 'GITLAB_FEATURES' => 'container_scanning,sast' do
       example.run
     end
