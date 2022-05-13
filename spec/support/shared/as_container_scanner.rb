86515363f2ee9089139916b889cb46ca78f50996
Brian Williams <bwilliams@gitlab.com>
2022-05-13 08:30:06 -0500

    Revert "Use `ee?` instead of `empty?`"
    This reverts commit cc62cf605d5d3c195214076f69f776e876a6df7b.

--- spec/support/shared/as_container_scanner.rb
+++ spec/support/shared/as_container_scanner.rb
@@ -17,7 +17,7 @@ RSpec.shared_examples 'as container scanner' do |item|
   end
 
   specify do
-    skip 'remediations are EE only' unless Gcs::Environment.ee?
+    skip 'remediations are EE only' if env['GITLAB_FEATURES'].empty?
 
     expect(report['remediations']).to be_present
 
